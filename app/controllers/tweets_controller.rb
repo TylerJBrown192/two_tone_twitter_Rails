78f6e1d09de526485aab7ce8ffcf66ec9cd0e21a
Sean Holstrom and Tyler Brown <basementbebop@gmail.com>
2015-03-30 16:04:44 -0700

    Added proper routes to new tweet and styling

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,32 @@
 class TweetsController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
 
+  def show
+    @user = User.find(params[:user_id])
+    @tweet = @user.tweets
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @tweet = @user.tweets.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @tweet = current_user.tweets.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = "Thanks for tweeting!"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+
+private
+  def tweet_params
+    params.require(:tweet).permit(:body, :user_id)
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(params[:user])
+    if @user.update(user_params)
       flash[:notice] = "Updated user information!"
       redirect_to users_path
     else
